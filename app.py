bb63ea287712ec6b538c191a9753d87e57624d32
Test User <test@example.com>
2026-09-09 13:29:36 +0900

    Migrate to SQLAlchemy for Postgres database

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-todos = []
+# Use POSTGRES_URL if available (Vercel Postgres), otherwise fallback to local SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URL', 'sqlite:///todo.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
+    todos = Todo.query.order_by(Todo.id).all()
     return render_template('index.html', todos=todos)
 
 @app.route('/add', methods=['POST'])
 def add():
-    todo = request.form.get('todo')
+    todo_task = request.form.get('todo')
+    if todo_task:
+        new_todo = Todo(task=todo_task)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/toggle/<int:todo_id>')
+def toggle(todo_id):
+    todo = db.session.get(Todo, todo_id)
     if todo:
-        todos.append({'task': todo, 'done': False})
+        todo.done = not todo.done
+        db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/toggle/<int:index>')
-def toggle(index):
-    if 0 <= index < len(todos):
-        todos[index]['done'] = not todos[index]['done']
-    return redirect(url_for('index'))
-
-@app.route('/delete/<int:index>')
-def delete(index):
-    if 0 <= index < len(todos):
-        todos.pop(index)
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = db.session.get(Todo, todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
